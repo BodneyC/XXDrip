ed7f706fb5d4c2c6552a800321fb19b52e733a41
BodneyC <benjcarrington@gmail.com>
2017-08-27 13:22:57 +0100

    Added bytes either side support

--- hex_out.cc
+++ hex_out.cc
@@ -1,35 +1,49 @@
 #include "hex_out.h"
- 
-
-
+/*-------------------------------------------------------------
+Preamble to formatted hexoutput
+-------------------------------------------------------------*/
 int hexbegin (int j, std::ostream &stream, int rowNum) {
   // Output table outline
   stream << "Hexadecimal Output" << '\n';
-  stream << '\n' << "        0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f  : 0 1 2 3 4 5 6 7 8 9 a b c d e f" << '\n' << "        ----------------------------------------------- : -------------------------------";
+  stream << '\n' << "        0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f  : 0 1 2 3 4 5 6 7 8 9 a b c d e f" << '\n' << "        ----------------------------------------------- : -------------------------------" << '\n';
   // Fill empty space for the columns
   for (int i = 0; i < rowNum; i++) {
+    stream << std::internal << std::setfill('0') << std::hex << std::showbase << std::setw(5) << j << " |                                                 :                                " << '\n';
     j += 16;
-    stream << "                                                :                                " << '\n' << std::internal << std::setfill('0') << std::hex << std::showbase << std::setw(5) << j << " | ";
   }
   return j;
 }
-//----------------------------------------------------------
-void hexoutput (BYTE sector[], std::ostream &stream, int initJ, int bytecount, int mode) {
-  int i = 0; //Overall byte count
-  int k = 0; //col count
-  int a = 0; //Char count
-  static int j = initJ+1;
-
-  if (j % 512 == 0) {
-    stream << '\n' << "        ----------------------------------------------- : -------------------------------";
+/*-------------------------------------------------------------
+If a single ilne of both sides missing bytes
+-------------------------------------------------------------*/
+void hexLT16out (BYTE *sector, std::ostream &stream, int initJ, int start, int bytecount) {
+  stream << std::hex << std::showbase << std::setw(5) << initJ << " | ";
+  for (int i = 0; i < start; i++)
+    stream << "   ";
+  for (int i = 0; i < bytecount; i++)
+    stream << std::hex << std::noshowbase << std::setw(2) << (int)sector[i] << ' ';
+  for (int i = 0; i < 16 - (start + bytecount); i++)
+    stream << "   ";
+  stream << ": ";
+  for (int i = 0; i < start; i++)
+    stream << "  ";
+  for (int i = 0; i < bytecount; i++) {
+    if((sector[i] <= 0x20) || (sector[i] == 0x7F)) {
+      stream << ". ";
+    } else {
+      stream << sector[i] << ' ';
+    }
   }
-  j--;
-  stream << '\n'<< std::hex << std::showbase << std::setw(5) << j << " | ";
+  stream << '\n';
+}
+/*-------------------------------------------------------------
+Primary line-by-line outputter
+-------------------------------------------------------------*/
+void hexoutput (BYTE *sector, std::ostream &stream, int initJ, int bytecount, int mode) {
+  static int j = initJ;
 
-  // if (bytecount < 16) {
-  //   k = (16 - bytecount)+1;
-  //   // Fill empty space for the rows
-  // }
+  stream << std::hex << std::showbase << std::setw(5) << j << " | ";
+
   j +=16;
   if (mode == 1) {
     for (int i = 0; i < 16-bytecount; i++) {
@@ -57,43 +71,10 @@ void hexoutput (BYTE sector[], std::ostream &stream, int initJ, int bytecount, i
       stream << sector[o] << ' ';
     }
   }
-/*
-  j += 16;
-  while( i != bytecount )	{
-    if (k != 16) { //Print one char, repeat
-      stream << std::hex << std::noshowbase << std::setw(2) << (int)sector[i] << ' ';
-      k++;
-    } else {
-      // stream << std::hex << std::noshowbase << std::setw(2) << (int)sector[i] << ' ';
-      if (mode == 2) {
-        for (size_t o = 0; o < (16 - bytecount)+1; o++) {
-          stream << "   ";
-        }
-        stream << ": ";
-      } else {
-        // stream << std::hex << std::noshowbase <<  std::setw(2) << (int)sector[i] << " : ";
-        stream << " : ";
-      }
-      i-= (bytecount-1);
-      if (mode == 1) {
-        for (size_t o = 0; o < 16 - bytecount; o++) {
-          stream << "  ";
-        }
-      }
-      for (a=0; a < bytecount; a++) {//char loop
-        if((sector[i] <= 0x20) || (sector[i] == 0x7F)) {
-          stream << ". ";
-        } else {
-          stream << sector[i] << ' ';
-        }
-        i++; //Compensate for subbing 15
-      }
-      i--;
-      k = 0; //For the new line
-    }
-    i++;
+  stream << '\n' ;
+  if (j % 512 == 0) {
+    stream << "        ----------------------------------------------- : -------------------------------" << '\n';
   }
-*/
 }
 
 /*
--- hex_out.h
+++ hex_out.h
@@ -10,5 +10,6 @@ typedef unsigned char BYTE;
 
 int hexbegin (int j, std::ostream &stream, int rowNum);
 void hexoutput (BYTE sector[], std::ostream &stream, int j, int bytecount, int mode);
+void hexLT16out (BYTE *sector, std::ostream &stream, int initJ, int start, int bytecount);
 
 #endif
