d6443a6b3efd3340224757396d51599046d0a744
BodneyC <benjcarrington@gmail.com>
2017-08-27 13:34:16 +0100

    Significant changes to raw outputting

--- main.cc
+++ main.cc
@@ -36,11 +36,13 @@ void returnArgs (int argn, char const *args[]) {
 	try {
 		TCLAP::CmdLine cmd("XXDrip", ' ', "0.01");
 		TCLAP::ValueArg<std::string> inputArg("i", "input", "Input File-path", true, "empty", "String");
-		TCLAP::ValueArg<std::string> outputArg("o", "output", "Output to File-path as Binary", false, "empty", "String");
+		TCLAP::ValueArg<std::string> outputArg("o", "output", "Output to File-path", false, "empty", "String");
 		TCLAP::ValueArg<int> beginArg("s", "start", "Offset of Begin Read", false, 0, "Int");
 		TCLAP::ValueArg<int> endArg("e", "end", "Offset of End Read", false, 0, "Int");
 		TCLAP::SwitchArg rawSwitch("r", "raw", "Output Raw Hex", cmd, 0);
+		TCLAP::SwitchArg binSwitch("b", "binary", "Output in binary mode", cmd, 0);
 		cmd.add(inputArg);
+		cmd.add(outputArg);
 		cmd.add(beginArg);
 		cmd.add(endArg);
 		cmd.parse(argn, args);
@@ -49,6 +51,7 @@ void returnArgs (int argn, char const *args[]) {
 		CMDArgs.outputString = outputArg.getValue();
 		CMDArgs.endVal = endArg.getValue();
 		CMDArgs.rawoutput = rawSwitch.getValue();
+		CMDArgs.binoutput = binSwitch.getValue();
 	} catch (TCLAP::ArgException &e) {
 		std::cerr << "Error: " << e.error() << " for arg " << e.argId() << std::endl;
 	}
@@ -59,100 +62,86 @@ MAIN
 int main(int argc, char const *argv[]) {
 	BYTE *sectorArr = new BYTE[512];
 	BYTE *lineArr = new BYTE[16];
-	BYTE *presec, *postsec;
 	bool overline = false;
 	returnArgs(argc, argv);
+	// outputFile if needed
 	std::ofstream outputFile;
-	// Open inputFile and check if open
 	std::ifstream inputFile (CMDArgs.inputString.c_str(), std::ios::binary);
+	// Open inputFile and check if open
 	if(!inputFile.is_open()) {
 		std::cout << "Cannot open input file" << '\n';
 	}
-	// Open output file as binary if CMDArgs.binoutput
-	if (CMDArgs.outputString != "empty") {
-		CMDArgs.rawoutput = true;
+	// Open output file as binary if requested...
+	if (CMDArgs.outputString != "empty" && CMDArgs.binoutput == true) {
+		CMDArgs.rawoutput = true; // If outputting as binary, also output as raw
 		outputFile.open (CMDArgs.outputString.c_str(), std::ios::binary | std::ios::trunc);
+		if(!outputFile.is_open()) {
+			std::cout << "Cannot open output file as binary" << '\n';
+		}
+	// If not as binary, output formatted
+	} else if (CMDArgs.outputString != "empty" && CMDArgs.binoutput == false) {
+		outputFile.open (CMDArgs.outputString.c_str(), std::ios::trunc);
 		if(!outputFile.is_open()) {
 			std::cout << "Cannot open output file" << '\n';
 		}
 	}
-	std::ostream &stream = CMDArgs.outputString == "empty" ? std::cout : outputFile;
+	std::ostream &stream = CMDArgs.outputString == "empty" ? std::cout : outputFile; // Set stream to cout or outputFile depending
 	// If endVal hasn't been provided, set endVal to EOF
 	if(!CMDArgs.endVal) {
 		inputFile.seekg (0, inputFile.end);
 		CMDArgs.endVal = inputFile.tellg();
 	}
-	// Set inputFile pointer to beginVal
-	inputFile.seekg(CMDArgs.beginVal, inputFile.beg);
-	// Process arguments with filebased beginVal
-	processArgs();
-	presec = new BYTE[CMDArgs.bytestoline];
-	postsec = new BYTE[CMDArgs.bytesafterline];
-
-
-	// if (CMDArgs.diff > (16 - (inputFile.tellg() % 16))) overline = true;
+	inputFile.seekg(CMDArgs.beginVal, inputFile.beg);	// Set inputFile pointer to beginVal
+	processArgs();	// Process arguments with filebased beginVal
 	if ((inputFile.tellg() % 16) + (CMDArgs.diff % 16) > 16 || CMDArgs.diff > 16) overline = true;
-
+	// If output is not meant as raw...
 	if(!CMDArgs.rawoutput) {
-		hexInfo.beginningJ = hexbegin(hexInfo.beginningJ, std::cout, hexInfo.rowNumBeg);
-
+		hexInfo.beginningJ = hexbegin(hexInfo.beginningJ, stream, hexInfo.rowNumBeg); // Output starting formatting
 		inputFile.read ((char*)lineArr, 16);
-		// std::cout << "CHEESE" << CMDArgs.loopcount << '\n';
-
-		// if (overline) {
-			if (inputFile.tellg() % 16 == 0 && CMDArgs.diff < 16) {
-				hexoutput(lineArr, std::cout, hexInfo.beginningJ, hexInfo.colNumEnd, 2);
-			} else if (inputFile.tellg() % 16 > 0) {
-				hexoutput(lineArr, std::cout, hexInfo.beginningJ, 16-hexInfo.colNumBeg, 1);
-			} else if (inputFile.tellg() % 16 == 0 && CMDArgs.diff >= 16) {
-				hexoutput(lineArr, std::cout, hexInfo.beginningJ, 16, 0);
-			}
-
-			if(overline) {
-				for (int i = 0; i < (CMDArgs.loopcount)-1; i++) {
-					inputFile.read ((char*)lineArr, 16);
-					hexoutput(lineArr, std::cout, 0, 16, 0);
-				}
-				if (CMDArgs.diff % 16 != 0) {
-					inputFile.read ((char*)lineArr, 16);
-					hexoutput(lineArr, std::cout, hexInfo.beginningJ, hexInfo.colNumEnd, 2);
-				}
-			}
-
-		// } else {
-			// hexoutput(lineArr, std::cout, hexInfo.beginningJ, 16-hexInfo.colNumEnd, 2);
-		// }
-		std::cout << '\n' << '\n' << "Program complete" << '\n';
-
-
-
-
-
-
-	} else {
-
-
-		// if (CMDArgs.outputString != "empty") stream = outputFile;
-
-
-		// Read the no of bytes before sec line
-		inputFile.read ((char*)presec, CMDArgs.bytestoline);
-		// For the no of bytes before sec line, print
-		for (size_t i = 0; i < CMDArgs.bytestoline; i++)
-			std::cout << (int)presec[i];
-		// Read for the no of remaining sectors...
-		for (size_t i = 0; i < CMDArgs.loopcount; i++) {
-			inputFile.read ((char*) sectorArr, 512);
-			// ...and write per byte
-			for (size_t p = 0; p < 512; p++)
-				std::cout << (int)sectorArr[i];
+		// Section for first-liners
+		if (inputFile.tellg() % 16 == 0 && CMDArgs.diff < 16) {
+			hexoutput(lineArr, stream, hexInfo.beginningJ, hexInfo.colNumEnd, 2); // Post-value whitespace
+		} else if (inputFile.tellg() % 16 == 0 && CMDArgs.diff >= 16) {
+			hexoutput(lineArr, stream, hexInfo.beginningJ, 16, 0); // Full first-line
+		} else if (inputFile.tellg() % 16 > 0 && CMDArgs.diff > 16 - (inputFile.tellg() % 16)) {
+			hexoutput(lineArr, stream, hexInfo.beginningJ, 16-hexInfo.colNumBeg, 1); // Pre-value whitespace, more than one line
+		} else {
+			hexLT16out(lineArr, stream, hexInfo.beginningJ, hexInfo.colNumBeg, CMDArgs.diff); // Pre-value whitespace, single line
 		}
-		// Read the no of bytes after sec line
-		inputFile.read ((char*) postsec, CMDArgs.bytesafterline);
-		// For the no of bytes after sec line, print
-		for (size_t i = 0; i < CMDArgs.bytesafterline; i++)
-			std::cout << (int)presec[i];
+		// If the output goes over the 16-byte mark, loop and output
+		if(overline) {
+			for (int i = 0; i < (CMDArgs.loopcount)-1; i++) {
+				inputFile.read ((char*)lineArr, 16);
+				hexoutput(lineArr, stream, 0, 16, 0);
+			}
+			if (CMDArgs.diff % 16 != 0) {
+				inputFile.read ((char*)lineArr, 16);
+				hexoutput(lineArr, stream, hexInfo.beginningJ, hexInfo.colNumEnd, 2);
+			}
+		}
+	// If not outputting as raw
+	} else {
+		for (int i = 0; i < CMDArgs.diff / 16; i++) {
+			inputFile.read ((char*)lineArr, 16);
+			if (CMDArgs.binoutput == true) {
+				stream.write((char*)lineArr, 16); // If binary/raw out, write as binary
+			} else {
+				for (int p = 0; p < 16; p++)
+					stream << (int)lineArr[p]; // If not binary but raw out, write as plaintext
+			}
+		}
+		// Handling the last portion of a line
+		inputFile.read ((char*)lineArr, 16);
+		if (CMDArgs.binoutput == true) {
+			stream.write((char*)lineArr, CMDArgs.diff % 16);
+		} else {
+			for (int i = 0; i < CMDArgs.diff % 16; i++)
+				stream << (int)lineArr[i];
+		}
 	}
-	delete[] sectorArr; delete[] presec; delete[] postsec;
+	std::cout << '\n' << "Program complete" << '\n';
+	// Closing handles and freeing memory
+	inputFile.close(); outputFile.close();
+	delete[] sectorArr; delete[] lineArr;
 	return 0;
 }
